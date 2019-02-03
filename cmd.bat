1498418f91764ab07652e3a73d52fe54d1d2e9ab
jumpin65 <jumpin65@hotmail.com>
2019-02-03 16:23:20 +1100

    Delete cmd.bat

--- cmd.bat
+++
@@ -1,7 +0,0 @@
-SET PATH=C:\Users\p742516\Desktop\tenacej\Ruby193\bin;%PATH%
-
-SET RUBYPATH=C:\Users\p742516\Desktop\tenacej\Ruby193\bin
-SET RUBYLIB=C:\Users\p742516\Desktop\tenacej\Ruby193\lib
-
-cmd.exe
-pause
